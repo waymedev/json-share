94ac5665339023852eb0d377cc5106e008b7cde6
waymedev <chenweimi.me@gmail.com>
2025-05-21 15:02:39 +0800

    docker 支持环境变量&修复部署问题

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,8 +1,45 @@
-import tailwindcss from '@tailwindcss/vite'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
+import { viteEnvs } from "vite-envs";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
-})
+  plugins: [
+    vue(),
+    tailwindcss(),
+    viteEnvs({
+      /*
+       * Uncomment the following line if `.env` is gitignored in your project.
+       * This enables you to use another file for declaring your variables.
+       */
+      declarationFile: ".env.example",
+
+      /*
+       * This is completely optional.
+       * It enables you to define environment
+       * variables that are computed at build time.
+       */
+      computedEnv: async ({ resolvedConfig /*declaredEnv, dotEnvLocal*/ }) => {
+        const path = await import("path");
+        const fs = await import("fs/promises");
+
+        const packageJson = JSON.parse(
+          await fs.readFile(
+            path.join(resolvedConfig.root, "package.json"),
+            "utf-8"
+          )
+        );
+
+        /*
+         * Here you can define any arbitrary value they will be available
+         * in `import.meta.env` and it's type definitions.
+         * You can also compute defaults for variable declared in `.env` files.
+         */
+        return {
+          BUILD_TIME: Date.now(),
+          VERSION: packageJson.version,
+        };
+      },
+    }),
+  ],
+});
